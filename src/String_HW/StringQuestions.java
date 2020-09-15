56da1de5124c9281963a297b9129dd3cb1d1d7b8
ajminef95 <kawsar.184@gmail.com>
2020-09-15 18:52:18 -0400

    First homework

--- src/String_HW/StringQuestions.java
+++ src/String_HW/StringQuestions.java
@@ -4,7 +4,45 @@ public class StringQuestions {
 
     public static void main(String[] args) {
 
-        String name = "peace required";
-        System.out.println("Message is: " + name);
+        byte noOfHoursInADay = 24;
+
+        System.out.println("Number of hours in a day: " + noOfHoursInADay);
+
+        short noOfDaysInAYear = 365;
+
+        System.out.println("Number of days in a year: " + noOfDaysInAYear);
+
+        int noOfEmployees = 500;
+
+        System.out.println("Number of employees in an organization: " + noOfEmployees);
+
+        long countryPopulation = 9000000000L;
+
+        System.out.println("Population in a country is: " + countryPopulation);
+
+        float interestRate = 2.3f;
+
+        System.out.println("Interest rate is: " + interestRate);
+
+        double accBalance = 374.25;
+
+        System.out.println("Balance in a bank account: " + accBalance);
+
+        boolean sunrisefromthewest = false;
+
+        System.out.println("Does the sun rise from the west: " + sunrisefromthewest);
+
+        char firstinitial = 'A';
+        char secondinitial = 'J';
+
+        System.out.println("Initials of my name are: " + firstinitial + secondinitial);
+
+        String fullName = "Ajmine Joy";
+
+        System.out.println("My full name is: " + fullName);
+
+
     }
+
+
 }
