df0ded607db10217ba8360a72ffea00b48a15787
ajminef95 <kawsar.184@gmail.com>
2020-09-20 13:39:22 -0400

    2nd homework

---
+++ src/String_HW/SecondHomework.java
@@ -0,0 +1,87 @@
+package String_HW;
+
+public class SecondHomework {
+
+    public static void main(String[] args) {
+
+       double fTemp = 68;
+        // cTemp = (fTemp - 32) × 5/9
+
+       double cTemp = (fTemp - 32) * 5/9;
+
+       System.out.println(fTemp + " Fahrenheit is equal to " + cTemp + " Celsius ");
+
+       double fTemp2 = 60;
+       //kTemp = (T(°F) + 459.67)× 5/9
+
+       double kTemp = (fTemp2 + 459.67) * 5/9;
+
+       System.out.println(fTemp2 + " Fahrenheit is equal to " + kTemp + " Kelvin ");
+
+       double kTemp2 = 300;
+       // cTemp = kTemp - 273.15
+
+       double cTemp2 = kTemp2 - 273.15;
+
+       System.out.println(kTemp2 + " Kelvin is equal to " + cTemp2 + " Celsius ");
+
+       double kTemp3 = 300;
+       // fTemp = kTemp × 9/5 - 459.67
+
+       double fTemp3 = kTemp3 * 9/5 - 459.67;
+
+       System.out.println(kTemp3 + " Kelvin is equal to " + fTemp3 + " Fahrenheit ");
+
+       double cTemp4 = 20;
+       // fTemp = cTemp × 9/5 + 32
+
+       double fTemp4 = cTemp4 * 9/5 + 32;
+
+       System.out.println(cTemp4 + " Celsius is equal to " + fTemp4 + " Fahrenheit ");
+
+       double cTemp5 = 20;
+       // kTemp = cTemp + 273.15
+
+       double kTemp4 = cTemp + 273.15;
+
+       System.out.println(cTemp5 + " Celsius is equal to " + kTemp4 + " Kelvin ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
